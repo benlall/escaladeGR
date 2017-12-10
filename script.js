d67fea00f953d3d5e69fb8b5a2a1f2513cd1cac7
piafben <benlall@hotmail.fr>
2017-12-10 17:57:27 +0100

    modif mise en page documents à telecharger

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 $('.item').css('min-height',$('.item').height()); /*permet de fixer la hauteur du carousel*/
 
+ /* page scroll on click */
+$(function(){   
+
+        var scroll = function() {   
+            $('.page-scroll a').bind('click', function(event) {
+                var $anchor = $(this);
+                $('html, body').stop().animate({
+                    scrollTop: $($anchor.attr('href')).offset().top
+                }, 1000, 'easeInOutExpo');
+                event.preventDefault();
+            });       
+        } 
+    
+        scroll()
+            
+});
