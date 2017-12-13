8c5dc779ce8b93343b29af16bba3a58bf3aadf1d
piafben <benlall@hotmail.fr>
2017-12-13 16:06:49 +0100

    script : archives et smooth scroll + bouton footer + correction orthographe

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 
-/*$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("li a").on('click', function(event) {
+$(document).ready(function(){
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+    $("li a, #returnTop").on("click", function(event){
+    
+        event.preventDefault();
+        var hash = this.hash;
+        
+        $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+        
+    });
 
-      // Store hash
-      var hash = this.hash;
+});
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+
+$(document).ready(function(){
+
+    $("#archives").on("click", function(event){
+    
+        alert("page à venir");
+
+	});
 });
